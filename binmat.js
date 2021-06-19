c39683bd9e839f5c4e5066b0ab197fdf61bf46dd
matr1x-hackmud <53655672+matr1x-hackmud@users.noreply.github.com>
2021-06-19 03:30:30 +0100

    Update binmat.js

--- binmat.js
+++ binmat.js
@@ -3,7 +3,7 @@ function(c,a){
   u = #hs.sys.upgrades({full:1}).filter(_=>!_.loaded).map(_=>_.i),
   g = #hs.accts.balance()
   
-  for(let i = 0; i < 5; i++) #ms.cash.xfer_gc_to({to:A, amount:Math.max(g, 32e9) })
+  for(let i = 0; i < 5; i++) #ms.cash.xfer_gc_to({to:A, amount:Math.min(g, 32e9) })
   #ms.sys.manage({unload:u})
   #ls.sys.xfer_upgrade_to({to:A, sn:u })
   
