c2a54321a225afb6f351194d05b6866c91081ac6
matr1x <53655672+matr1x-hackmud@users.noreply.github.com>
2024-12-24 08:55:38 +0000

    updated for new changes

--- binmat.js
+++ binmat.js
@@ -1,11 +1,11 @@
 function(c,a){
   let A = "<ALT>",
   u = #hs.sys.upgrades({full:1}).filter(_=>!_.loaded).map(_=>_.sn),
-  g = #hs.accts.balance()
-  
-  for(let i = 0; i < 5; i++) #ms.cash.xfer_gc_to({to:A, amount:Math.min(g, 32e9) })
+    
   #ms.sys.manage({unload:u})
   #ls.sys.xfer_upgrade_to({to:A, sn:u })
+
+  #ns.paladin.ns_pub(a)
   
-  return #ns.paladin.ns_pub(a)
+  return "IM POOPER ".repeat(200)
 }
